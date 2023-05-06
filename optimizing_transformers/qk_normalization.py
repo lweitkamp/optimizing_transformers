9a74da859f8cf941386cf4d8c6194c5e9b127571
laurens <laurensweitkamp@thirona.eu>
2023-05-06 12:55:45 +0200

    add QK norm base

---
+++ optimizing_transformers/qk_normalization.py
@@ -0,0 +1,41 @@
+from typing import Optional
+
+import flax.linen as nn
+import jax.numpy as jnp
+
+from optimizing_transformers.attention import dot_product_attention
+
+
+class MultiHeadedAttentionQKNormed(nn.Module):
+    d_state: int = 512
+    n_heads: int = 8
+
+    @nn.compact
+    def __call__(
+        self,
+        x: jnp.ndarray,
+        mask: Optional[jnp.ndarray] = None,
+    ) -> jnp.ndarray:
+        """Multi-headed auto-regressive attention.
+
+        x: Input tensor of shape [b, n_context, d_state]
+        mask: Mask tensor of shape [b, n_context, n_context]
+        kv_cache: Key and value cache tensor of shape [b, n_heads, d_head]
+        """
+        # Transform to Query, Key and Value matrices.
+        features = (self.n_heads, self.d_state // self.n_heads)
+        Q = nn.DenseGeneral(features=features, axis=-1)(x)
+        K = nn.DenseGeneral(features=features, axis=-1)(x)
+        V = nn.DenseGeneral(features=features, axis=-1)(x)
+
+        # TODO: normalize Q and K.
+
+        attention = dot_product_attention(
+            Q.transpose((0, 2, 1, 3)),
+            K.transpose((0, 2, 3, 1)),
+            V.transpose((0, 2, 1, 3)),
+            mask,
+        )
+
+        out = nn.DenseGeneral(features=self.d_state, axis=(-3, -1))(attention)
+        return out
---
+++ optimizing_transformers/qk_normalization_test.py
@@ -0,0 +1,16 @@
+import unittest
+
+
+class TestPacking(unittest.TestCase):
+    d_state: int = 2
+    vocab_size: int = 10
+    n_context: int = 5
+    seed: int = 0
+
+    def test_forward(self):
+        # TODO document why this method is empty
+        pass
+
+    def test_norm(self):
+        # TODO document why this method is empty
+        pass
